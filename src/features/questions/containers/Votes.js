4e98efcaf33138da61e51209ada8a78d5bff99c8
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-05-30 15:22:34 +0200

    Adds triangle mixin

--- src/features/questions/containers/Votes.js
+++ src/features/questions/containers/Votes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { connect } from "react-redux";
 
 import { createUpvote, createDownvote } from "../actions/questions_actions";
@@ -9,15 +9,57 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
+const Triangle = styled.div`
+  width: 0;
+  height: 0;
+  ${props => {
+    if (props.direction === "up") {
+      return css`
+        border-left: ${props.size} solid transparent;
+        border-right: ${props.size} solid transparent;
+        border-bottom: ${props.size} solid ${props.color};
+      `;
+    } else if (props.direction === "right") {
+      return css`
+        border-top: ${props.size} solid transparent;
+        border-bottom: ${props.size} solid transparent;
+        border-left: ${props.size} solid ${props.color};
+      `;
+    } else if (props.direction === "down") {
+      return css`
+        border-left: ${props.size} solid transparent;
+        border-right: ${props.size} solid transparent;
+        border-top: ${props.size} solid ${props.color};
+      `;
+    } else if (props.direction === "left") {
+      return css`
+        border-top: ${props.size} solid transparent;
+        border-bottom: ${props.size} solid transparent;
+        border-right: ${props.size} solid ${props.color};
+      `;
+    }
+  }};
+`;
+
 class Votes extends Component {
   renderVote = () => {
     if (this.props.question) {
       const { id } = this.props.question;
       return (
         <React.Fragment>
-          <span onClick={() => this.props.createUpvote(id)}>UP</span>
+          <Triangle
+            direction={"up"}
+            size={"14px"}
+            color={"#858c93"}
+            onClick={() => this.props.createUpvote(id)}
+          />
           <span>{this.props.num_answers}</span>
-          <span onClick={() => this.props.createDownvote(id)}>DOWN</span>
+          <Triangle
+            direction={"down"}
+            size={"14px"}
+            color={"#858c93"}
+            onClick={() => this.props.createDownvote(id)}
+          />
         </React.Fragment>
       );
     }
