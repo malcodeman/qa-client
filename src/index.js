f5253d3727f6398bdd31be1a73437c048d93eaee
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-07-22 18:24:47 +0200

    Adds themes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter as Router } from "react-router-redux";
+import { ThemeProvider } from "styled-components";
 
 import "./index.css";
 import store from "./core/state/store";
 import history from "./core/state/history";
 import App from "./core/App";
+import defaultTheme from "./core/themes/default";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
     </Router>
   </Provider>,
   document.getElementById("root")
