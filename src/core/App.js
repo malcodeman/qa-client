4e1d1fde7a098a98e590df00c2058fded39e4f28
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-07-22 20:00:55 +0200

    Gets theme from users -> me reducer

--- src/core/App.js
+++ src/core/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
+import lightTheme from "./style/themes/light";
+import darkTheme from "./style/themes/dark";
 import routes from "./routes";
 
-const App = () => {
-  return (
-    <Switch>
-      {routes.map(route => {
-        if (route.private && localStorage.getItem("token") === null) {
-          return <Redirect key={route.path} to="/" />;
-        }
-        return (
-          <Route
-            key={route.path}
-            path={route.path}
-            exact={route.exact}
-            component={route.component}
-          />
-        );
-      })}
-    </Switch>
-  );
+class App extends Component {
+  getTheme = theme => {
+    switch (theme) {
+      case "light":
+        return lightTheme;
+      case "dark":
+        return darkTheme;
+      default:
+        return lightTheme;
+    }
+  };
+  render() {
+    const { me } = this.props;
+    return (
+      <ThemeProvider theme={me !== null ? this.getTheme(me.theme) : lightTheme}>
+        <Switch>
+          {routes.map(route => {
+            if (route.private && localStorage.getItem("token") === null) {
+              return <Redirect key={route.path} to="/" />;
+            }
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.component}
+              />
+            );
+          })}
+        </Switch>
+      </ThemeProvider>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    me: state.users.me
+  };
 };
 
-export default App;
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(App)
+);
--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter as Router } from "react-router-redux";
-import { ThemeProvider } from "styled-components";
 
 import "./index.css";
 import store from "./core/state/store";
 import history from "./core/state/history";
 import App from "./core/App";
-import defaultTheme from "./core/themes/default";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <ThemeProvider theme={defaultTheme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </Router>
   </Provider>,
   document.getElementById("root")
