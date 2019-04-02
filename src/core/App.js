743615955e34b0287a443642e4e1194ab3165a49
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-04-02 15:48:26 +0200

    Simplifies theming

--- src/core/App.js
+++ src/core/App.js
@@ -1,57 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import lightTheme from "./style/themes/light";
-import darkTheme from "./style/themes/dark";
+import defaultTheme from "./styles/themes/default";
 import routes from "./routes";
+import store from "./state/store";
 
-class App extends Component {
-  getTheme = theme => {
-    switch (theme) {
-      case "light":
-        return lightTheme;
-      case "dark":
-        return darkTheme;
-      default:
-        return lightTheme;
-    }
-  };
-  render() {
-    const { me } = this.props;
-    return (
-      <ThemeProvider theme={me !== null ? this.getTheme(me.theme) : lightTheme}>
-        <Switch>
-          {routes.map(route => {
-            if (route.private && localStorage.getItem("token") === null) {
-              return <Redirect key={route.path} to="/" />;
-            }
-            return (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.component}
-              />
-            );
-          })}
-        </Switch>
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={defaultTheme}>
+        <Router>
+          <Switch>
+            {routes.map(route => {
+              if (route.private && localStorage.getItem("token") === null) {
+                return <Redirect key={route.path} to="/" />;
+              }
+              return (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              );
+            })}
+          </Switch>
+        </Router>
       </ThemeProvider>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    me: state.users.me
-  };
+    </Provider>
+  );
 };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    null
-  )(App)
-);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
-import store from "./core/state/store";
+import "./core/styles/index.css";
 import App from "./core/App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
