4c31f2a53323c8868ce8f90fd7a596d227c69c59
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-04-13 10:50:49 +0200

    Removes duplicate auth styling components

--- src/features/auth/components/SignupForm.js
+++ src/features/auth/components/SignupForm.js
@@ -1,54 +1,19 @@
 import React from "react";
 import * as Yup from "yup";
-import styled from "styled-components";
-import { withFormik, Form, Field } from "formik";
+import { withFormik } from "formik";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 import Loader from "../../loader/components/Loader";
 import { signup } from "../actions/authActionCreators";
 import { validateEmail, validateUsername } from "../sagas/authSagas";
-
-const StyledForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-  padding: 24px 0;
-`;
-
-const FormItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 24px;
-`;
-
-const Input = styled(Field)`
-  height: 36px;
-  font-size: 0.8rem;
-  padding: 0 4px;
-  outline: 0;
-  background-color: ${props => props.theme.backgroundSecondary};
-  border-radius: ${props => props.theme.borderRadius};
-  border: 1px solid ${props => props.theme.borderColor};
-  color: ${props => props.theme.primary};
-`;
-
-const Button = styled.button`
-  color: #fff;
-  border: 0;
-  cursor: pointer;
-  height: 36px;
-  font-size: 0.8rem;
-  padding: 0;
-  margin-bottom: 24px;
-  background-color: ${props => props.theme.brand};
-  border-radius: ${props => props.theme.borderRadius};
-`;
-
-const ErrorMessage = styled.span`
-  margin: 4px 0;
-  font-size: 0.8rem;
-  color: ${props => props.theme.error};
-`;
+import {
+  StyledForm,
+  FormItem,
+  Input,
+  ErrorMessage,
+  Button
+} from "../styles/authStyles";
 
 const FormikForm = props => {
   const { errors, touched, isSubmitting } = props;
